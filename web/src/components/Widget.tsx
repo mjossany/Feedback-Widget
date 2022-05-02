40c14d773d38fee8bdf32d4e1fda6b3fc7b937eb
Jossany Moura <mjossany@gmail.com>
2022-05-02 19:56:09 -0300

    Create and implement state to show and hide the widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,23 @@
 import { ChatTeardropDots } from "phosphor-react"
+import { useState } from "react"
 
 export function Widget() {
+  const [ isWidgetOpen, setIsWidgetOpen ] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
   return (
-    <button>
-      <ChatTeardropDots />
-    </button>
+    <div className="absolute bottom-5 right-5">
+      { isWidgetOpen ? <p>Hello World!</p> : null }
+      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </button>
+    </div>
   )
 }
