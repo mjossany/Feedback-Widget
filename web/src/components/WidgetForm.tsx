7f309689f931228be7772bf9fcc2ebdc8118610a
Jossany Moura <mjossany@gmail.com>
2022-05-03 15:05:33 -0300

    Create logic to change the Widget when the user chooses the type of feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -41,22 +41,28 @@ export function WidgetForm() {
         <span className="text-xl leading-6">Deixe seu feedback</span>
         <CloseButton />
       </header>
-      <div className="flex py-8 gap-2 w-full">
-        {
-          Object.entries(feedbackTypes).map(([key, value]) => {
-            return(
-              <button
-                key={key}
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                onClick={ () => setFeedbackType(key as FeedbackType) }
-              >
-                <img src={value.image.source} alt={value.image.alt} />
-                <span>{value.title}</span>
-              </button>
-            )
-          })
-        }
-      </div>
+      {
+        !feedbackType ? (
+          <div className="flex py-8 gap-2 w-full">
+            {
+              Object.entries(feedbackTypes).map(([key, value]) => {
+                return(
+                  <button
+                    key={key}
+                    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                    onClick={ () => setFeedbackType(key as FeedbackType) }
+                  >
+                    <img src={value.image.source} alt={value.image.alt} />
+                    <span>{value.title}</span>
+                  </button>
+                )
+              })
+            }
+          </div>
+        ) : (
+          <p>Thank you for your feedback!</p>
+        )
+      }
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
