bf2596c276e3ee1e5289637a901b6507f4c34725
Jossany Moura <mjossany@gmail.com>
2022-05-03 14:56:54 -0300

    Set stylization for WidgetForm buttons

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from '../assets/bug.svg';
@@ -29,6 +31,8 @@ const feedbackTypes = {
 }
 
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState(null)
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -39,7 +43,11 @@ export function WidgetForm() {
         {
           Object.entries(feedbackTypes).map(([key, value]) => {
             return(
-              <button>
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                onClick={ () => setFeedbackType(key) }
+              >
                 <img src={value.image.source} alt={value.image.alt} />
                 <span>{value.title}</span>
               </button>
@@ -47,7 +55,6 @@ export function WidgetForm() {
           })
         }
       </div>
-      <p>Hello World!</p>
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
