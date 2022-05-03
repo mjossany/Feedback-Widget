c911eb35ff6e3db303e5e60909ee1ec6306e3a1a
Jossany Moura <mjossany@gmail.com>
2022-05-03 17:48:59 -0300

    Create components to divide the WidgetForm logic/code

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -30,7 +31,7 @@ const feedbackTypes = {
   }
 }
 
-type FeedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
@@ -43,22 +44,7 @@ export function WidgetForm() {
       </header>
       {
         !feedbackType ? (
-          <div className="flex py-8 gap-2 w-full">
-            {
-              Object.entries(feedbackTypes).map(([key, value]) => {
-                return(
-                  <button
-                    key={key}
-                    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                    onClick={ () => setFeedbackType(key as FeedbackType) }
-                  >
-                    <img src={value.image.source} alt={value.image.alt} />
-                    <span>{value.title}</span>
-                  </button>
-                )
-              })
-            }
-          </div>
+          <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
         ) : (
           <p>Thank you for your feedback!</p>
         )
