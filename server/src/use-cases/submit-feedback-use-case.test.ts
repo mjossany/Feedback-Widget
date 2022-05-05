c9aa8ca6fdaefd98c8a0a6ca5aea6c55dccec240
Jossany Moura <mjossany@gmail.com>
2022-05-05 14:55:37 -0300

    Create tests

---
+++ server/src/use-cases/submit-feedback-use-case.test.ts
@@ -0,0 +1,40 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {} },
+  { sendMail: async () => {} }
+)
+
+describe('Submit feedback', () => {
+  it('should be able to submit a feedback', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64,45235623462345',
+    })).resolves.not.toThrow();
+  });
+
+  it('should not be able to submit feedback without type', async () => {
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64,45235623462345',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit feedback without comment', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64,45235623462345',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit feedback with an invalid screenshot', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'example comment',
+      screenshot: 'test.jpg',
+    })).rejects.toThrow();
+  });
+});
