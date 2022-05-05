a472d5e3f216e1b37c542e9854ee977afa6f650a
Jossany Moura <mjossany@gmail.com>
2022-05-04 23:02:44 -0300

    .

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,25 +1,18 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
-var transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "420cfd7540a88f",
-    pass: "49396eac1657e3"
-  }
-});
-
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot} = req.body;
   
   const prismaFeedbacksRepository = new PrismaFeedbackRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter()
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
   )
 
   await submitFeedbackUseCase.execute({
