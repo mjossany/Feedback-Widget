30093d0f0d7d8e76c7a93e28a49ed474bd4cb07e
Jossany Moura <mjossany@gmail.com>
2022-05-05 18:22:21 -0300

    Start the button stylization

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,25 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
+import AppLoading from 'expo-app-loading';
+
+import { 
+  Inter_400Regular,
+  Inter_500Medium,
+  useFonts } from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,
