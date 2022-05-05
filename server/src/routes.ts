3e644887373d8f5bf5b5fdabb5cf24a0d7ddf7e5
Jossany Moura <mjossany@gmail.com>
2022-05-04 22:48:42 -0300

    .

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { prisma } from './prisma';
 import nodemailer from 'nodemailer';
+import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
@@ -16,19 +17,14 @@ var transport = nodemailer.createTransport({
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot} = req.body;
   
-  const feedback = 
+  const prismaFeedbacksRepository = new PrismaFeedbackRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  )
 
-  await transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Jossany Moura <mjossany@gmail.com>',
-    subject: 'Novo feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
+  await submitFeedbackUseCase.execute({
+    type, comment, screenshot,
   })
 
-  return res.status(201).json({ data: feedback });
+  return res.status(201).send();
 });
