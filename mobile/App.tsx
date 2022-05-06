260f19129c84fd618e520f6228cca39e7f96a93a
Jossany Moura <mjossany@gmail.com>
2022-05-06 14:04:13 -0300

    Install and set up react native reanimated and gesture handler

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { Widget } from './src/components/Widget';
