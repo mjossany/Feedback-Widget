604eefcb2ffecb7a9e14ef2bc49bcf9ce4a3f146
Jossany Moura <mjossany@gmail.com>
2022-05-06 14:38:31 -0300

    Implement and stylize Copyright component

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -5,6 +5,7 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { Options } from '../Options';
 
 function Widget() {
 
@@ -33,7 +34,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-
+        <Options />
       </BottomSheet>
     </>
   );
