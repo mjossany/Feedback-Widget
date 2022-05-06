41e18396edbfb2f745a75ed79b4b4429525b6c8c
Jossany Moura <mjossany@gmail.com>
2022-05-06 15:11:31 -0300

    Add Title and Option to Options component

--- mobile/src/components/Option/styles.ts
+++ mobile/src/components/Option/styles.ts
@@ -6,7 +6,7 @@ export const styles = StyleSheet.create({
     width: 104,
     height: 112,
     justifyContent: 'center',
-    alignItem: 'center',
+    alignItems: 'center',
     padding: 8,
     borderRadius: 8,
     marginHorizontal: 8,
