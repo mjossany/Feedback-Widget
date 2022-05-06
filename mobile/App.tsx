7b20e654a3bf992a6edf2da6bebb451522e5b4b6
Jossany Moura <mjossany@gmail.com>
2022-05-06 14:16:30 -0300

    Create BottomSheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -26,12 +26,12 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
       <StatusBar 
         style="light"
         backgroundColor='transparent'
         translucent
       />
+      <Widget />
     </View>
   );
 }
