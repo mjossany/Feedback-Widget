5b99d63ff5d479207a2d7b9fe77561c645d80546
Jossany Moura <mjossany@gmail.com>
2022-05-07 12:05:48 -0300

    Install body-parser and increase the limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors())
-app.use(express.json());
+app.use(bodyParser.json({ limit: '3mb' }));
 app.use(routes);
 
 app.listen(3333, () => {
