4d6fe0e4b1a637afa8177464a994de15bceed6a8
Jossany Moura <mjossany@gmail.com>
2022-05-09 12:47:19 -0300

    Change the footer text

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -64,7 +64,7 @@ export function WidgetForm() {
         </>
       )}
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+        Made with ♥ by <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
     </div>
   )
